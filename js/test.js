135e7a5c78161dead34ce030054c00bacce61d18
ZWS <nooldey@qq.com>
2022-02-09 13:33:41 +0800

    test: commit file from remote vscode

---
+++ js/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zws 
+ * @Date: 2022-02-09 13:32:28 
+ * @Last Modified by: zws
+ * @Last Modified time: 2022-02-09 13:33:17
+ */
+function test(){
+  console.log("file from github remote edit")
+}
